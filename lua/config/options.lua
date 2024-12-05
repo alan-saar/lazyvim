37e1830d796fe3725fe80573b9f6200ba0d4ddc3
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-12-05 13:27:09 -0300

    hledger funcional

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.mapleader = ","
+
+-- não instalou o Treesitter ledger automaticamente, instalei com :TSInstall ledger
+-- o commentário do ledger não estava funcionando, fiz um autocmd para o tipo do ledger (.journal)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "ledger" },
+  command = "setlocal commentstring=;%s",
+})
