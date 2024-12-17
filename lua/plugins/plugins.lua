eefe2a4f6e38e9d36dad67516d237799e1ac9159
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2024-12-17 10:25:51 -0300

    Ranger no lazyvim

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -14,4 +14,18 @@ return {
   },
   "tpope/vim-vinegar",
   "Leonidas-from-XIV/memento-mori.nvim",
+  {
+    "kelly-lin/ranger.nvim",
+    config = function()
+      require("ranger-nvim").setup({
+        replace_netrw = false,
+      })
+      vim.api.nvim_set_keymap("n", "<leader>fm", "Ranger [f]ile [manager]", {
+        noremap = true,
+        callback = function()
+          require("ranger-nvim").open(true)
+        end,
+      })
+    end,
+  },
 }
