3f518cd45747f0823aa403f64882b764426ed2a7
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2025-04-19 08:18:24 -0300

    adiciona python nos extras e desabilita autoformat

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- autoformat desabilitado
+-- para habilitar na sessão <leader>uf
+-- para habilitar numa linguagem específica ver a discussão:
+-- https://github.com/LazyVim/LazyVim/discussions/141 para
+vim.g.autoformat = false
