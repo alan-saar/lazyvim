04defef13e0500192ea425f2198ed208543760b4
Alan Pancieri Berger Saar <alan.berger@ufes.br>
2026-08-28 10:24:03 -0300

    merge com o nvim do main

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,12 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- { import = "lazyvim.plugins.extras.dap.core" },
+    -- { import = "lazyvim.plugins.extras.test.core" },
+    -- { import = "lazyvim.plugins.extras.coding.neogen" },
+    -- { import = "lazyvim.plugins.extras.editor.harpoon2" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    -- { import = "lazyvim.plugins.extras.util.rest" },
     -- import/override with your plugins
     { import = "plugins" },
   },
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,33 @@
 -- Options are automatically loaded before lazy.nvim startup.
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
 require("config.remote_clipboard").setup()
 
 vim.opt.relativenumber = false
 vim.g.autoformat = false
 
--- minhas configurações
--- virgula como leader
+-- daqui para baixo é meu
 vim.g.mapleader = ","
 vim.g.maplocalleader = ","
+
+-- não instalou o Treesitter ledger automaticamente, instalei com :TSInstall ledger
+-- o commentário do ledger não estava funcionando, fiz um autocmd para o tipo do ledger (.journal)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "ledger" },
+  command = "setlocal commentstring=;%s",
+})
+
+-- vim better whitespaces
+vim.g.better_whitespace_enabled = 0 -- desabilitei porque não gosto do vermelho, prefiro do jeito do kickstart
+vim.g.strip_whitespace_on_save = 1 -- esse aqui só funciona setando na marra, não consegui usando meu comando :AutoFormatEnableTrailing
+
+-- memento mori
+vim.g.memento_mori_birthdate = "1982-07-23"
+-- comando para mostrar github.com/alan-saar/mementoMori no firefox
+vim.api.nvim_create_user_command(
+  "MementoMoriWeb",
+  "!firefox ~/workspace/mementoMori/simple/memento_mori.html",
+  { bang = true }
+)
+
+vim.opt.spelllang = { "en", "br", "pt_br" }
